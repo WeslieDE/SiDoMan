98badaa7f6d55f372f01d57d3030e246e33fa8bb
Christopher <git@clatza.dev>
2020-10-22 00:15:18 +0200

    add theme

--- pages/error.php
+++ pages/error.php
@@ -1,10 +1,11 @@
 <?php
     $HTML = new HTML();
-    $HTML->importHTML("style/default/error.html");
+    //$HTML->importHTML("style/default/error.html");
+    $HTML->importHTML("style/default/user.html");
 
-	$HTML->setHTMLTitle("Error");
+	$HTML->setHTMLTitle("Error 404");
 
     $HTML->build();
-    header("HTTP/1.0 404 Not Found");
+    //header("HTTP/1.0 404 Not Found");
     echo $HTML->ausgabe();
 ?>
