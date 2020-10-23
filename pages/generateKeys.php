4e74f5e9a2c4a4bda23aa73f919b335cdee365bc
Christopher <git@clatza.dev>
2020-10-23 23:39:31 +0200

    add login check

--- pages/generateKeys.php
+++ pages/generateKeys.php
@@ -1,16 +1,21 @@
 <?php
 
-if($_REQUEST['MASTERKEY'] != $RUNTIME['SYSTEMKEY'])
-    die("ACCESS DENIED!");
-    
-$dockerClient = new Docker();
-$allContainers = $dockerClient->getAllContainers();
-
-foreach($allContainers as $thisContainer)
+if($_REQUEST['MASTERKEY'] == $RUNTIME['SYSTEMKEY'])
 {
-    $apiKey	= calcAPIKey($thisContainer);
-
-    echo $thisContainer['Name']." == ".$apiKey."<br>\n";
+    $dockerClient = new Docker();
+    $allContainers = $dockerClient->getAllContainers();
+    
+    foreach($allContainers as $thisContainer)
+    {
+        $apiKey	= calcAPIKey($thisContainer);
+    
+        echo $thisContainer['Name']." == ".$apiKey."<br>\n";
+    }
+    die();
 }
+
+die("ACCESS DENIED!");
+
+
 
 ?>
