3b4dabbfa63a22d6732774c0293909a05e08d835
Christopher <git@clatza.dev>
2020-10-23 14:16:07 +0200

    add first version of dashboard

---
+++ pages/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_SESSION['LOGIN']))
+{
+    if($_SESSION['LOGIN'] == 'true')
+    {
+        $dockerClient = new Docker();
+        $allContainers = $dockerClient->getAllContainers();
+        $container = NULL;
+
+        foreach($allContainers as $thisContainer)
+        {
+            if($thisContainer->Id == $_REQUEST['CONATINER'] || $thisContainer->Names[0] == ("/".$_REQUEST['CONATINER']))
+                $container = $thisContainer
+        }
+
+        if($container == NULL)
+            die("unknown container");
+        
+        $HTML = new HTML();
+        $HTML->setHTMLTitle($container->Names[0]);
+        $HTML->importHTML("style/default/dashboard.html");
+        
+        $HTML->build();
+        echo $HTML->ausgabe();
+    }else{
+        die("Login is not valid!");
+    }
+}else{
+    die("Access denied!");
+}
+
+
+?>
