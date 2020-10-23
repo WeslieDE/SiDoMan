12663cf793a19fc43c51423ebc3a40cc6c182583
Christopher <git@clatza.dev>
2020-10-23 22:09:52 +0200

    remove \r

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -3,7 +3,7 @@
 function clean($string)
 {
     $string = str_replace("", "", $string);
-    $string = str_replace("\r", "\n", $string);
+    $string = str_replace("\r", "", $string);
     $string = str_replace("\n\n", "\n", $string);
     return preg_replace('#\\x1b[[][^A-Za-z]*[A-Za-z]#', '', $string);
 }
