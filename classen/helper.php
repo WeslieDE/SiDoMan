0620d1eb14269c6046cf9a50fef945fe286ca63e
Christopher <git@clatza.dev>
2020-10-23 23:44:31 +0200

    calc api key with docker hash

--- classen/helper.php
+++ classen/helper.php
@@ -11,15 +11,6 @@ function calcAPIKey($container)
 {
     global $RUNTIME;
 
-    return md5($RUNTIME['SYSTEMKEY'].md5(trim(ltrim($container['Names']['0'], '/'))));
+    return md5($container['Id']);
 }
-
-if(!file_exists("./pages/systemkey.txt"))
-{
-	$randomKey	=	md5(rand(111111111, 999999999));
-	file_put_contents("./pages/systemkey.txt", $randomKey);
-}
-
-$RUNTIME['SYSTEMKEY'] = file_get_contents("./pages/systemkey.txt");
-
 ?>
