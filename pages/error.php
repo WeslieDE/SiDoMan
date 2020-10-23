7463feb8efdb108ca05dda6a1c431c6bf70cda8a
Christopher <git@clatza.dev>
2020-10-23 14:21:58 +0200

    ADD ERROR PAGE

--- pages/error.php
+++ pages/error.php
@@ -1,11 +1,10 @@
 <?php
     $HTML = new HTML();
-    //$HTML->importHTML("style/default/error.html");
-    $HTML->importHTML("style/default/user.html");
+    $HTML->importHTML("style/default/error.html");
 
 	$HTML->setHTMLTitle("Error 404");
 
     $HTML->build();
-    //header("HTTP/1.0 404 Not Found");
+    header("HTTP/1.0 404 Not Found");
     echo $HTML->ausgabe();
 ?>
